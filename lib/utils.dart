49489ba6b3e91aa12de198bd9b3b4dbbd0277165
Shreeman <shreemanarjunsahu@gmail.com>
2021-08-16 21:57:20 +0530

    fixes

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,4 +1,4 @@
 var appID = "8f3811d69fae4880b9666df00c7f1f54";
 
 
-const token = '0068f3811d69fae4880b9666df00c7f1f54IAB9aLc3RGAyAtDDU0O/yEh0P+wvR7Uxhrk14oGTeMnaPQx+f9gAAAAAEACcjToMzYgYYQEAAQDLiBhh';
+const token = '0068f3811d69fae4880b9666df00c7f1f54IAB5bgS/jME9m/nA3klRfhSaLn3sezIjtIwongmN9SNxxwx+f9gAAAAAEAA5Pyg0ca8bYQEAAQBxrxth';
